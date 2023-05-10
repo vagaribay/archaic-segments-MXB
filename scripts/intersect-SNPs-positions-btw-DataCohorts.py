d423377d0c1999eb84e04e944112805b571c4c39
Valeria Añorve-Garibay <112778297+vagaribay@users.noreply.github.com>
2023-05-10 13:13:19 -0400

    Create intersect-SNPs-positions-btw-DataCohorts.py

---
+++ scripts/intersect-SNPs-positions-btw-DataCohorts.py
@@ -0,0 +1,28 @@
+__author__ = 'vgaribay'
+import argparse
+
+USAGE = """
+find_shared_sites_btw_data_cohorts.py -v1
+                -v2
+                -o
+"""
+
+import argparse, pandas as pd
+
+parser = argparse.ArgumentParser(description="Find share sites between data cohorts..")
+
+parser.add_argument('-v1', '--vcf1', action="store", dest="data_1", help="", default=None, type=str)
+parser.add_argument('-v2', '--vcf2', action="store", dest="data_2", help="", default=None, type=str)
+parser.add_argument('-o', '--out', action="store", dest="out_file", help="", default=None, type=str)
+
+args = parser.parse_args()
+
+data_1 = pd.read_csv(args.data_1, sep = "\t", header = None, usecols = [0,3,4,5],
+                     names = ["CHROM", "POS", "REF", "ALT"])
+
+data_2 = pd.read_csv(args.data_2, sep = "\t", header = None, usecols = [0,3,4,5],
+                  names = ["CHROM", "POS", "REF", "ALT"])
+
+intersection = pd.merge(data_2, data_1, how='inner', on=['CHROM','POS', 'REF', 'ALT'])
+
+intersection.to_csv(args.out_file, sep='\t', index=False, header=False)
